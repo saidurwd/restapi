f734e1fc5640ea154b8534dcd90d7d8a5cbccd4f
Saidur Rahman <saidurwd@gmail.com>
2023-07-02 22:05:36 +0600

    conf update

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=webapi',
+    'dsn' => 'mysql:host=localhost;dbname=restapi',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
